3326615d8a7d33c168290f163b766f1986f59af5
Pandit <Sushil.Pandit@franklintempleton.com>
2023-04-13 19:44:10 +0530

    initial project commit

---
+++ src/main/java/Constants/TimeOuts.java
@@ -0,0 +1,7 @@
+package Constants;
+
+public class TimeOuts {
+	public static long DEFAULT_TIMEOUT=60;
+	public static int SWITCH_WINDOW_TIMEOUT=30;
+
+}
---
+++ src/main/java/quickstart/Pages/GoogleSearchPage.java
@@ -0,0 +1,55 @@
+package quickstart.Pages;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import Constants.TimeOuts;
+
+public class GoogleSearchPage  {
+
+	WebDriver driver;
+	Properties properties;
+	
+	
+	public GoogleSearchPage(WebDriver driver) throws IOException {
+		this.driver=driver;
+		this.properties= new Properties();
+		String currDir= System.getProperty("user.dir");
+		InputStream input= new FileInputStream(currDir+"\\src\\main\\java\\quickstart\\Pages\\GoogleSearchPage.properties");
+		System.out.println("---------------------check the second directory----------------"+input);
+		this.properties.load(input);
+	}
+
+//	 This method is for entering a keyword in an input field on the webpage
+	
+	
+	public void enterKeyWord(String keyword) {
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@name='q']")));
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+		driver.findElement(By.xpath("//textarea[@name='q']")).sendKeys(keyword);
+		driver.findElement(By.xpath("//textarea[@name='q']")).sendKeys(Keys.ENTER);
+	}
+
+	public void waitForResultLink(String keyword) {
+		String searchResultLink="//h3[contains(.,'"+keyword+"')]";
+		WebDriverWait wait= new WebDriverWait(driver, TimeOuts.DEFAULT_TIMEOUT);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(searchResultLink)));		
+	}
+
+	public  void searchKeyword(String keyword) {
+
+		this.enterKeyWord(keyword);
+		this.waitForResultLink(keyword);	
+		
+	}
+
+
+}
